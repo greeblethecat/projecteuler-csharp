9b8f41697bc06cedfe96960014e6cda41ed26aba
greeble <greeblethecat@gmail.com>
2021-08-12 09:23:28 -0700

    +0004

--- lib/Solution0001-0010.cs
+++ lib/Solution0001-0010.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Collections.Generic;
 
-public class Solution0001 : Solution
-{
+public class Solution0001 : Solution {
   override public string run() {
     int answer = 0;
     for (int i = 0; i < 1000; i++) {
@@ -13,8 +13,7 @@ public class Solution0001 : Solution
   }
 }
 
-public class Solution0002 : Solution
-{
+public class Solution0002 : Solution {
   public override string run() {
     int answer = 2;
     int f_2 = 1, f_1 = 2, f0 = 3;
@@ -28,5 +27,31 @@ public class Solution0002 : Solution
       f0 = f_2 + f_1;
     }
     return answer.ToString();
+  }
+}
+
+public class Solution0003 : Solution {
+  public override string run() {
+    long answer = 0;
+    long num = 600851475143;
+    long curNum = num;
+    while(curNum > 1) {
+      for (int i = 2; i <= curNum; i++) {
+        if (curNum%i == 0) {
+          if (i > answer) {
+            answer = i;
+          }
+          curNum /= i;
+        }
+      }
+    }
+
+    return answer.ToString();
+  }
+}
+
+public class Solution0004 : Solution {
+  public override string run() {
+    return base.run();
   }
 }
